53efa1567002b0cbfba353882652e20997ab9372
Pixar <oncwnuLkhSYS5JThzectGiNoUN54@git.weixin.qq.com>
2024-11-01 23:28:26 +0800

    能够记录测试用例的返回时间，但是记录测试用例运行的内存记录仍没有实现

--- src/main/java/com/pixar/pixojcodesanbox/JavaNativeCodeSandbox.java
+++ src/main/java/com/pixar/pixojcodesanbox/JavaNativeCodeSandbox.java
@@ -2,11 +2,14 @@ package com.pixar.pixojcodesanbox;
 
 import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.io.resource.ResourceUtil;
+import cn.hutool.core.util.StrUtil;
 import com.pixar.pixojcodesanbox.model.ExecuteCodeRequest;
 import com.pixar.pixojcodesanbox.model.ExecuteCodeResponse;
 import com.pixar.pixojcodesanbox.model.ExecuteMessage;
+import com.pixar.pixojcodesanbox.model.JudgeInfo;
 import com.pixar.pixojcodesanbox.utils.ProcessUtils;
 import org.springframework.util.ResourceUtils;
+import org.springframework.util.StopWatch;
 
 import javax.annotation.Resource;
 import java.io.BufferedReader;
@@ -15,9 +18,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.lang.reflect.Array;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 public class JavaNativeCodeSandbox implements CodeSandbox {
     public static final String GLOBAL_CODE_DIR_NAME = "tmpCode";
@@ -28,7 +33,7 @@ public class JavaNativeCodeSandbox implements CodeSandbox {
         JavaNativeCodeSandbox javaNativeCodeSandbox = new JavaNativeCodeSandbox();
         ExecuteCodeRequest executeCodeRequest = new ExecuteCodeRequest();
         executeCodeRequest.setInputList(Arrays.asList("1 2", "1 3"));
-        String code = ResourceUtil.readStr("testCode/simpleComputeArgs/Main.java", StandardCharsets.UTF_8);
+        String code = ResourceUtil.readStr("testCode/simpleCompute/Main.java", StandardCharsets.UTF_8);
         executeCodeRequest.setCode(code);
         executeCodeRequest.setLanguage("java");
         ExecuteCodeResponse executeCodeResponse = javaNativeCodeSandbox.executeCode(executeCodeRequest);
@@ -66,18 +71,58 @@ public class JavaNativeCodeSandbox implements CodeSandbox {
         }
 
         // 执行代码
+
+        List<ExecuteMessage> executeMessageList = new ArrayList<>();
         for (String inputArgs : inputList) {
-            System.out.println(userCodeParentPath);
+            StopWatch stopWatch = new StopWatch();
             String runCmd = String.format("java -Dfile.encoding=GBK -cp %s Main %s", userCodeParentPath, inputArgs);
             // System.out.println(runCmd);
             try {
                 Process runProcess = Runtime.getRuntime().exec(runCmd);
                 ExecuteMessage executeMessage = ProcessUtils.runProcessAndGetMessage(runProcess, "运行");
+                // ExecuteMessage executeMessage = ProcessUtils.runInteractProcessAndGetMessage(runProcess, inputArgs);
                 System.out.println(executeMessage);
+                executeMessageList.add(executeMessage);
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
         }
-        return null;
+
+        // 收集整理输出结果
+        ExecuteCodeResponse executeCodeResponse = new ExecuteCodeResponse();
+        List<String> outputList = new ArrayList<>();
+        // 默认用例使用的最长时间，便于讨论是否超时
+        long maxTime = 0;
+        for (ExecuteMessage executeMessage : executeMessageList) {
+            String errorMessage = executeMessage.getErrorMessage();
+            if (StrUtil.isNotBlank(errorMessage)) {
+                executeCodeResponse.setMessage(errorMessage);
+                // 执行中存在错误
+                executeCodeResponse.setStatus(3);
+                // todo 定义状态枚举值
+                break;
+            }
+            outputList.add(executeMessage.getMessage());
+            Long time = executeMessage.getTime();
+            if( time != null ){
+                maxTime = Math.max(maxTime,time);
+            }
+        }
+        executeCodeResponse.setOutputList(outputList);
+        // 没有错误的话 将状态值设置为 1
+        if(outputList.size() == executeMessageList.size()){
+            executeCodeResponse.setStatus(1);
+        }
+        JudgeInfo judgeInfo  = new JudgeInfo();
+
+        // todo 可以设置每个测试用例都有一个独立时间和内存
+        judgeInfo.setTime(maxTime);
+        // todo 需要借助第三方库获取java程序的内存占用
+        // judgeInfo.setMemory();
+
+        executeCodeResponse.setJudgeInfo(judgeInfo);
+
+
+        return executeCodeResponse;
     }
 }
--- src/main/java/com/pixar/pixojcodesanbox/utils/ProcessUtils.java
+++ src/main/java/com/pixar/pixojcodesanbox/utils/ProcessUtils.java
@@ -1,9 +1,10 @@
 package com.pixar.pixojcodesanbox.utils;
 
+import cn.hutool.core.util.StrUtil;
 import com.pixar.pixojcodesanbox.model.ExecuteMessage;
+import org.springframework.util.StopWatch;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 
 /**
  * 进程工具类
@@ -19,7 +20,8 @@ public class ProcessUtils {
     public static ExecuteMessage runProcessAndGetMessage(Process runProcess, String opName) {
         ExecuteMessage executeMessage = new ExecuteMessage();
         try {
-            // 等待程序执行，直到程序运行完成得到一个错误码
+            StopWatch stopWatch = new StopWatch();
+            stopWatch.start();
             int exitValue = runProcess.waitFor();
             executeMessage.setExitValue(exitValue);
             //正常退出
@@ -59,6 +61,51 @@ public class ProcessUtils {
                 }
                 executeMessage.setErrorMessage(errorCompileOutputStringBuilder.toString());
             }
+            stopWatch.stop();
+            executeMessage.setTime(stopWatch.getLastTaskTimeMillis());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return executeMessage;
+    }
+
+    /**
+     * 执行交互式进程并获取信息
+     *
+     * @param runProcess 运行进程
+     * @param args       交互时输入的参数
+     * @return executeMessage
+     */
+
+    // todo 检测输入参数个数是否符合
+    public static ExecuteMessage runInteractProcessAndGetMessage(Process runProcess,  String args) {
+        ExecuteMessage executeMessage = new ExecuteMessage();
+        try {
+            // 向控制台输入程序
+            OutputStream outputStream = runProcess.getOutputStream();
+
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream);
+            String[] split = args.split(" ");
+            outputStreamWriter.write(StrUtil.join("\n", (Object) split)+"\n");
+            // 将输入上传并且清空缓存区
+            outputStreamWriter.flush();
+
+            // 分批获取用户正常输出
+            InputStream inputStream = runProcess.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            StringBuilder compileOutputStringBuilder = new StringBuilder();
+            // 逐行读取
+            String compileOutputLine;
+            while ((compileOutputLine = bufferedReader.readLine()) != null) {
+                compileOutputStringBuilder.append(compileOutputLine);
+            }
+            executeMessage.setMessage(compileOutputStringBuilder.toString());
+            // 资源回收释放
+            outputStreamWriter.close();
+            outputStream.close();
+            inputStream.close();
+            runProcess.destroy();
         } catch (Exception e) {
             e.printStackTrace();
         }
